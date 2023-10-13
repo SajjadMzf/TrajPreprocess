f3eb8feb2b5fd7b848c7de44a897945b9b6b56f8
Sajjad <sajjad.mozaffari@warwick.ac.uk>
2023-10-13 14:17:31 +0100

    added exid preprocess configs

--- PreprocessTraj.py
+++ PreprocessTraj.py
@@ -273,6 +273,7 @@ if __name__ == '__main__':
     #parser = argparse.ArgumentParser()
     #parser.add_argument('config_file', type=str)
     #args = parser.parse_args()
+    '''
     preprocess = PreprocessTraj(
             #args.config_file,
             'configs/highd_preprocess.yaml',
@@ -281,8 +282,8 @@ if __name__ == '__main__':
     preprocess.dataset_specific_preprocess()
     
     exit()
-    
-    for i in [2]: #3,4,6]:
+    '''
+    for i in [2,3,4,6]:
         preprocess = PreprocessTraj(
             #args.config_file,
             'configs/exid_preprocess{}.yaml'.format(i),
